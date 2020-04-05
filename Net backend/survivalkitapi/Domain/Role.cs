1424db26f88525c241e5a8f0f21f8bdcfbd4c26e
roman-stapunov <roman.stapunov@goldaim.ru>
2020-04-05 12:35:56 +0300

    Implement select roles

--- Net backend/survivalkitapi/Domain/Role.cs
+++ Net backend/survivalkitapi/Domain/Role.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Domain
 {
+    [Table("role")]
     public class Role
     {
+        [Column("id")]
         public int Id { get; set; }
+        
+        [Column("role")]
         public string RoleName { get; set; }
     }
 }
