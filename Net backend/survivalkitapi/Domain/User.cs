7bc54c90bceaa5d2474bbcee0f77d7c41d894a0f
roman-stapunov <roman.stapunov@goldaim.ru>
2020-04-05 11:01:06 +0300

    Add api solution

---
+++ Net backend/survivalkitapi/Domain/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Domain
+{
+    public class User
+    {
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public long Role { get; set; }
+    }
+}
---
+++ Net backend/survivalkitapi/survivalkitapi/Controllers/UserController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Domain;
+
+namespace survivalkitapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        private User _user;
+
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public ActionResult<User> Create(User user)
+        {
+            _user = user;
+            return CreatedAtAction("GetById", user.Name);
+        }
+
+        public string GetById(long id)
+        {
+            return _user.Name;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            if (_user != null)
+            {
+                return new List<User>() { _user };
+            }
+            else
+            {
+                return new List<User>()
+                {
+                    new User()
+                    {
+                        Id = 0,
+                        Name = "None",
+                        Email = "None",
+                        Password = "None",
+                        PhoneNumber = "None",
+                        Role = 0,
+                        UserName = "None"
+                    }
+                };
+            }
+        }
+    }
+}
